1148548fd6ed12c371be5b55a204df0883356ec4
unknown <lpc477@gmail.com>
2019-11-01 21:51:55 +0000

    inital commit

---
+++ autistica/mentalhealthform/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.db.models import Model
+# Create your models here.
+
+class MHFormData (models.Model):
+    formID = models.BigIntegerField();
+    username = models.BigIntegerField();
+    depression = models.IntegerField();
+    anxiety = models.IntegerField();
+    stress = models.IntegerField();
+    q1 =  models.PositiveSmallIntegerField();
+    q2 =  models.PositiveSmallIntegerField();
+    q3 =  models.PositiveSmallIntegerField();
+    q4 =  models.PositiveSmallIntegerField();
+    q5 =  models.PositiveSmallIntegerField();
+    q6 =  models.PositiveSmallIntegerField();
+    q7 =  models.PositiveSmallIntegerField();
+    q8 =  models.PositiveSmallIntegerField();
+    q9 =  models.PositiveSmallIntegerField();
+    q10 =  models.PositiveSmallIntegerField();
+    q11 =  models.PositiveSmallIntegerField();
+    q12 =  models.PositiveSmallIntegerField();
+    q13 =  models.PositiveSmallIntegerField();
+    q14 =  models.PositiveSmallIntegerField();
+    q15 =  models.PositiveSmallIntegerField();
+    q16 =  models.PositiveSmallIntegerField();
+    q17 =  models.PositiveSmallIntegerField();
+    q18 =  models.PositiveSmallIntegerField();
+    q19 =  models.PositiveSmallIntegerField();
+    q20 =  models.PositiveSmallIntegerField();
+    q21 =  models.PositiveSmallIntegerField();
