8eda48fc7bc496ff9174f5a53778c12346e3d49b
dillonryan98 <dillonryan98@gmail.com>
2019-11-02 03:32:26 +0000

    mental  health template page

--- autistica/frontend/src/App.js
+++ autistica/frontend/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div class="navbar">
+        <a class="active" href="#mentalHealth">Mental Health</a>
+      </div>
+
+
+       <div className = "SubGraph">
+       <div className = "subGraph1">
+          <p>Graph goes here</p>
+       </div>
+
+       <div className = "subGraph2">
+          <p>You're doing good, keep it up.</p>
+       </div>
+
+       </div>
+
+
       </header>
     </div>
+
   );
 }
+
 
 export default App;
