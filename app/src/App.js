84da88d699306031ffab3642bdddf44f8d54d443
Stephen Byrne <byrnes68@tcd.ie>
2019-11-02 02:01:11 +0000

    buttons page added
    buttons page w/routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,8 @@
 //import './App.css';
 
 import React from "react";
+import "./App.css"
+import 'bootstrap/dist/css/bootstrap.css' ;
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,13 +14,16 @@ export default function App() {
   return (
     <Router>
       <div>
-        <nav >
+        <nav>
           <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
             <li>
               <Link to="/mentalHealth">MentalHealth</Link>
+            </li>
+            <li>
+              <Link to="/settings">Settings</Link>
             </li>
           </ul>
         </nav>
@@ -27,6 +32,15 @@ export default function App() {
             renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/mentalHealth">
+            <MentalHealth />
+          </Route>
+          <Route path="/adjustments">
+            <MentalHealth />
+          </Route>
+          <Route path="/organisation">
+            <MentalHealth />
+          </Route>
+          <Route path="/settings">
             <MentalHealth />
           </Route>
           <Route path="/">
@@ -39,9 +53,44 @@ export default function App() {
 }
 
 function Home() {
-  return <h2>Home</h2>;
+
+  return (
+    //navbar should be at top -> dillon and klaudia
+    <div>
+      <UserProfile name="Stephen" />
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 text-center">
+          <Link to="/mentalHealth" type="button" class="btn">Mental Health</Link>
+        </div>
+        <div class="col-md-3 col-sm-6 text-center">
+          <Link to="/mentalHealth" type="button" class="btn">Adjustments</Link>
+        </div>
+        <div class="col-md-3 col-sm-6 text-center">
+          <Link to="/mentalHealth" type="button" class="btn">Organisation</Link>
+        </div>
+        <div class="col-md-3 col-sm-6 text-center">
+          <Link to="/mentalHealth" type="button" class="btn">Work Culture</Link>
+        </div>
+      </div>
+    </div>
+  </div>
+    //something to take up all empty space
+  );
 }
 
+function UserProfile(props){
+  return(
+    <div class="container">
+      <h2 class="text-center">Welcome Back {props.name} !</h2>
+    </div>
+  );
+}
+
+function Settings() {
+  return <h2>Settings</h2>;
+}
 function MentalHealth() {
   return <h2>MentalHealth</h2>;
 }
