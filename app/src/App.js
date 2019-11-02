86bfef37789b5726a13addc0a32b69833f6327ef
Stephen Byrne <byrnes68@tcd.ie>
2019-11-02 00:00:15 +0000

    Init routes
    Inital routes - focusing on mental health, working with brid on graphs also

---
+++ app/src/App.js
@@ -0,0 +1,47 @@
+//import './App.css';
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav >
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/mentalHealth">MentalHealth</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/mentalHealth">
+            <MentalHealth />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function MentalHealth() {
+  return <h2>MentalHealth</h2>;
+}
